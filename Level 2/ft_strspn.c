bb7343bb921d08029a0579bd23dc7a9770077946
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2023-12-05 17:11:37 +0100

    Add files via upload

---
+++ Level 2/ft_strspn.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strspn.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lribette <lribette@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/05 17:04:53 by lribette          #+#    #+#             */
+/*   Updated: 2023/12/05 17:10:39 by lribette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+size_t	ft_strspn(const char *s, const char *accept);
+
+int ft_strlen(const char *str)
+{
+	int i;
+	
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+size_t	ft_strspn(const char *s, const char *accept)
+{
+    int i;
+
+    i = 0;
+    if (s[i] == accept[i])
+    {
+        while (s[i] == accept[i])
+            i++;
+    }
+    return (i);
+}
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("   strspn = %ld\n", strspn(argv[1], argv[2]));
+	printf("ft_strspn = %ld\n", ft_strspn(argv[1], argv[2]));
+}
