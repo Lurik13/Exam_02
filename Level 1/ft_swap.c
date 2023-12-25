40802bfe8887f20484abceecff3a4cbf9002d565
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2023-12-25 15:37:06 +0100

    Add files via upload

---
+++ Level 1/ft_swap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lribette <lribette@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/25 15:29:16 by lribette          #+#    #+#             */
+/*   Updated: 2023/12/25 15:36:03 by lribette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_swap(int *a, int *b)
+{
+	int	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int	main(void)
+{
+	int	first = 15;
+	int	second = 2;
+	ft_swap(&first, &second);
+	printf("%d, %d", first, second);
+}
