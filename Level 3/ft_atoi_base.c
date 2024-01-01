1dcf216fb6104de68a3307f09b215fe742e52e78
Lucas Ribette <116387708+Lurik13@users.noreply.github.com>
2024-01-01 14:26:08 +0100

    Add files via upload

---
+++ Level 3/ft_atoi_base.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lribette <lribette@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 13:45:46 by lribette          #+#    #+#             */
+/*   Updated: 2024/01/01 14:24:41 by lribette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	value_of(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (c - '0');
+	if (c >= 'a' && c <= 'f')
+		return (c - 'a' + 10);
+	if (c >= 'A' && c <= 'F')
+		return (c - 'A' + 10);
+	return (0);
+}
+
+int	ft_atoi_base(const char *str, int str_base)
+{
+	int	i;
+	int	nega;
+	int	number;
+	
+	i = 0;
+	nega = 1;
+	number = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-')
+	{
+		nega *= -1;
+		i++;
+	}
+	while (str[i])
+	{
+		number = number * str_base + value_of(str[i]);
+		i++;
+	}
+	return (number);
+}
+
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%d", ft_atoi_base(argv[1], 16));
+}
